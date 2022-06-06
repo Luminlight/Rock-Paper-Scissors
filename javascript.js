0bbd11906fd5defb2bb601eda0b7c8f795d63341
Luminlight <razznikx@gmail.com>
2022-06-06 19:44:01 -0400

    Round is played based on clicking a button, currently logged into console

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
 const selectionButtons = document.querySelectorAll("[data-selection]")
-const choices = ["rock", "paper", "scissors"]
+const options = ["rock", "paper", "scissors"]
+
+function computerPlay() {
+    const randomPick = options[Math.floor(Math.random() * options.length)]
+    return randomPick
+}
+
 
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener("click", e => {
@@ -9,5 +15,21 @@ selectionButtons.forEach(selectionButton => {
 })
 
 function makeSelection(selection) {
-    console.log(selection)
+    const computerSelection = computerPlay()
+
+    console.log("You: " + selection)
+    console.log("Comp: " + computerSelection)
+    playRound(selection, computerSelection)
+}
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")) {
+            console.log("You win!")
+        } else if (playerSelection === computerSelection) {
+            console.log("A tie!")
+        } else {
+            console.log("You lost!")
+        }
 }
